116f0488c434670bea5a0bbfc1765fa58e2aacbe
VulkreoN <hakim.redjem@epitech.eu>
2022-11-11 14:30:56 +0100

    MODIFIED: CI

--- build.py
+++ build.py
@@ -127,7 +127,7 @@ elif os.name == 'posix':
         print("vcpkg is already installed\n")
     # install dependencies with vcpkg
     print("Installing dependencies with vcpkg...\n")
-    if vcpkg_install == False:
+    if not vcpkg_install:
         os.system("./vcpkg/vcpkg install sfml")
         os.system("./vcpkg/vcpkg install asio")
     else:
